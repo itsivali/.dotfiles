7758b7cfa0a45cd98cd7dfedc99fc8fd169f8e29
Willis Ivali <itsivali@outlook.com>
2024-07-03 21:40:11 +0300

    first commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Flakes by Willis";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+  {
+    nixosConfigurations = {
+      nixos = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+        ];
+      };
+    };
+
+    homeConfigurations = {
+      willis = home-manager.lib.homeManagerConfiguration {
+        inherit (nixpkgs) system;
+        modules = [
+          ./home.nix
+        ];
+      };
+    };
+  };
+}
