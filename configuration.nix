b33cf12abc083beb00d8b76ba20943823f7f2deb
Willis Ivali <itsivali@outlook.com>
2024-07-06 19:25:18 +0300

    made changes to config file to correct typo

--- configuration.nix
+++ configuration.nix
@@ -14,7 +14,7 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "prague"; # Define your hostname.
+  networking.hostName = "prague"; 
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
