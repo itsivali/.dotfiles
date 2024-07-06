35673991a67855fae01368e57e44a14e749f7f41
Willis Ivali <itsivali@outlook.com>
2024-07-06 19:37:03 +0300

    added garbage collection

--- configuration.nix
+++ configuration.nix
@@ -6,7 +6,8 @@
 
 {
   imports =
-    [ # Include the results of the hardware scan.
+    [
+      # Include the results of the hardware scan.
       ./hardware-configuration.nix
     ];
 
@@ -14,12 +15,34 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "prague"; 
+  networking.hostName = "prague";
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
   # networking.proxy.default = "http://user:password@proxy:port/";
   # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Configure Nix garbage collection
+  nix.gc = {
+    automatic = true; # Enable automatic garbage collection
+    dates = "weekly"; # Run garbage collection weekly
+    options = "--delete-older-than 5d"; # Keep generations for 5 days
+  };
+
+  # Enable systemd service to run garbage collection
+  systemd.services.nix-gc = {
+    description = "Nix Garbage Collection";
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig = {
+      Type = "oneshot";
+      ExecStart = "${pkgs.nix}/bin/nix-collect-garbage -d";
+    };
+    timerConfig = {
+      OnCalendar = "weekly";
+      Persistent = true;
+    };
+  };
+
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -70,9 +93,9 @@
     isNormalUser = true;
     description = "willis";
     extraGroups = [ "networkmanager" "wheel" ];
-    openssh.authorizedKeys.keys = [ "ssh-ed25519 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCO12JQaQVR62xfWCUAX8mw/i++Rfpj+j0BemAz1VdWjnLPsMHGdpLvSqp6oW+fW9TzvSyZWFdqOT86oMje4pM6GCcY7x1dlvoJTCs62YaQST0C1yn/nSzU3q3+lJcdlf7jnPQO1h2CzzTX/QRfx35iV3CCzNexbf36iqIpNg8Cm7QLIyk2u8iOPxaodwJDpuSsFcKGgG/iyAcUjJc//n8tR8YvwtmEelI8FMkDMs/UjI2CTZqm42dsgZy7A9g74JuourvDVMOwz+33Tk2tMZezrzV6U5s9k4H+CZPAIHJPa4NbqYYySWT7RWdBAKa0kUT3ZhjnDKpUkl7cXPAdAsYIq6M0abmOT7j7/mhY/EWAWD8uXqclvFOLQBXG+CSmcegH5YKiNdOF5UK1rncR5N6710M2a+ZzPgojWnSb/zoplF2nkACVhgND2tdhwj9hO7tnmmd/p1I3jx4Hs/C3M1eJ2h1FN0eWSBJHxg7OQyQn57cYiCOnGzoXWEdGAfrOrmiRdppZNoh+k5xGLPihIooZ6YKWkQqb7QjL1fgE1wDiQJUmGOgxo0ZF6smMVXjg/ixGASyAG77LcjBS+rzC5/8GLTI2ZAv9G85CFv+rCOWwDDdv+Chv0Xc6wDNvWcDrAZNG5O/6GYAZHaBMNFEK7xUPpCmtUJGJNj7JAWVlyK8fyw == <itsivali@outlook.com>"];
+    openssh.authorizedKeys.keys = [ "ssh-ed25519 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQCO12JQaQVR62xfWCUAX8mw/i++Rfpj+j0BemAz1VdWjnLPsMHGdpLvSqp6oW+fW9TzvSyZWFdqOT86oMje4pM6GCcY7x1dlvoJTCs62YaQST0C1yn/nSzU3q3+lJcdlf7jnPQO1h2CzzTX/QRfx35iV3CCzNexbf36iqIpNg8Cm7QLIyk2u8iOPxaodwJDpuSsFcKGgG/iyAcUjJc//n8tR8YvwtmEelI8FMkDMs/UjI2CTZqm42dsgZy7A9g74JuourvDVMOwz+33Tk2tMZezrzV6U5s9k4H+CZPAIHJPa4NbqYYySWT7RWdBAKa0kUT3ZhjnDKpUkl7cXPAdAsYIq6M0abmOT7j7/mhY/EWAWD8uXqclvFOLQBXG+CSmcegH5YKiNdOF5UK1rncR5N6710M2a+ZzPgojWnSb/zoplF2nkACVhgND2tdhwj9hO7tnmmd/p1I3jx4Hs/C3M1eJ2h1FN0eWSBJHxg7OQyQn57cYiCOnGzoXWEdGAfrOrmiRdppZNoh+k5xGLPihIooZ6YKWkQqb7QjL1fgE1wDiQJUmGOgxo0ZF6smMVXjg/ixGASyAG77LcjBS+rzC5/8GLTI2ZAv9G85CFv+rCOWwDDdv+Chv0Xc6wDNvWcDrAZNG5O/6GYAZHaBMNFEK7xUPpCmtUJGJNj7JAWVlyK8fyw == <itsivali@outlook.com>" ];
     packages = with pkgs; [
-    #  thunderbird
+      #  thunderbird
     ];
   };
 
@@ -140,7 +163,7 @@
   # networking.firewall.allowedTCPPorts = [ 80 443 ];
   # networking.firewall.allowedUDPPorts = [ ... ];
   # Or disable the firewall altogether.
-   networking.firewall.enable = true;
+  networking.firewall.enable = true;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
@@ -150,5 +173,5 @@
   # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "24.05"; # Did you read the comment?
 
-  nix.settings.experimental-features = ["nix-command"  "flakes"];
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
