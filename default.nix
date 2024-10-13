3c83c1a8faf06071846a8ffadc03747dee0c0a89
Willis Ivali <itsivali@outlook.com>
2024-10-13 14:38:38 +0300

    refactored default nix file

--- default.nix
+++ default.nix
@@ -1,17 +1,17 @@
-# default.nix
 { pkgs ? import <nixpkgs> { 
     config = {
-      allowUnfree = true;
+      allowUnfree = true; 
     }; 
   }
 }:
 
-#pkgs.mkShell {
- # buildInputs = [
-  #  pkgs.vim                  # Example free software
-   # pkgs.google-chrome         # Example nonfree software
-    #pkgs.spotify               # Another example of nonfree software
-    #pkgs.wget                  # Another free software example
-   # pkgs.ffmpeg                # Free software
-  #];
-#}
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    vim
+    google-chrome
+    spotify
+    notepad-next
+    ffmpeg
+    vlc
+  ];
+}
