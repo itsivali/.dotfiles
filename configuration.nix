b2f150d624846646d063cf95e1d37a02020c594e
installer <installer@gmail.com>
2024-10-14 21:21:14 +0300

    removed hyprland and enabled gnome

--- configuration.nix
+++ configuration.nix
@@ -5,21 +5,25 @@
       ./hardware-configuration.nix
     ];
 
+  # Permitted insecure packages
   nixpkgs.config.permittedInsecurePackages = [
     "docker-24.0.9"
   ];
 
+  # Bootloader configuration
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  # Hostname
   networking.hostName = "nixos";
 
+  # Garbage collection configuration
   nix.gc = {
     automatic = true;
     dates = "weekly";
     options = "--delete-older-than 7d";
   };
-
+  
   systemd.timers.nix-gc = {
     description = "Timer for Nix Garbage Collection";
     wantedBy = [ "timers.target" ];
@@ -29,56 +33,39 @@
     };
   };
 
+  # Automatic Upgrades
   system.autoUpgrade = {
     enable = true;
     allowReboot = false;
   };
 
+  # Enable networking
   networking.networkmanager.enable = true;
 
+  # Set the timezone
   time.timeZone = "Africa/Nairobi";
 
+  # Localization settings
   i18n.defaultLocale = "en_US.UTF-8";
 
-  # Enable Wayland and set up Hyprland as the window manager
+  # Enable the X11 windowing system
   services.xserver.enable = true;
-  services.xserver.displayManager.startx.enable = true;
 
-  environment.systemPackages = with pkgs; [
-    vim
-    wget
-    google-chrome
-    vscode
-    python3
-    nodejs_22
-    signal-desktop
-    telegram-desktop
-    bitwarden-desktop
-    git
-    spotify
-    mpv
-    fastfetch
-    filezilla
-    htop
-    obsidian
-    cider
-    localsend
-    bluemail
-    nixpkgs-fmt
-    nixd
-    discord
-    pipenv
-    ranger
-    docker
-    gh
-    anydesk
-    brave
-    neovim
-    hyprland
-  ];
+  # Enable GNOME
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
+  # X11 keymap settings
+  services.xserver = {
+    xkb = {
+      layout = "us";
+    };
+  };
+
+  # Enable CUPS printing service
   services.printing.enable = true;
 
+  # Enable PipeWire for sound
   hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
@@ -90,6 +77,7 @@
     pulse.enable = true;
   };
 
+  # User configuration
   users.users.willis = {
     isNormalUser = true;
     description = "willis";
@@ -100,17 +88,70 @@
     packages = with pkgs; [];
   };
 
+  # Install Firefox
   programs.firefox.enable = true;
 
+  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
+  # System-wide packages
+  environment.systemPackages = with pkgs; [
+    vim
+    wget
+    google-chrome
+    vscode
+    python3
+    nodejs_22
+    nodePackages_latest.npm
+    signal-desktop
+    telegram-desktop
+    gnome-extension-manager
+    bitwarden-desktop
+    git
+    spotify
+    mpv
+    fastfetch
+    filezilla
+    gnome-tweaks
+    htop
+    obsidian
+    cider
+    localsend
+    bluemail
+    nixpkgs-fmt
+    nixd
+    discord
+    streamlink-twitch-gui-bin
+    pipenv
+    python312Packages.pip
+    postman
+    python312Packages.pytest
+    ranger
+    docker
+    gh
+    gh-notify
+    anydesk
+    github-desktop
+    wpsoffice
+    docker
+    zoom-us
+    corefonts
+    vistafonts
+    brave
+    neovim
+    stremio
+  ];
+
+  # Firewall settings
   networking.firewall = {
     allowedTCPPorts = [ 80 443 53317 ];
     allowedUDPPorts = [ 80 443 53317 ];
     enable = true;
   };
 
+  # NixOS release version
   system.stateVersion = "24.05";
 
+  # Enable experimental features
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 }
