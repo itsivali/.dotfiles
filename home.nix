4200db4dddbbf6a18c5e109a1baac06ad8818389
Willis Ivali <itsivali@outlook.com>
2024-10-15 17:01:20 +0300

    fixed errors in home configuration

--- home.nix
+++ home.nix
@@ -10,28 +10,9 @@
     pkgs.hello
   ];
 
-<<<<<<< HEAD
-  programs.home-manager.enable = true;
+
   home.sessionVariables = {
     EDITOR = "vim";
-=======
-  # Home Manager needs a bit of information about you and the paths it should manage.
-  home.username = "willis";
-  home.homeDirectory = "/home/willis";
-
-  # Specify the state version for Home Manager.
-  home.stateVersion = "23.05"; # Set this to the correct version
-
-  # The home.packages option allows you to install Nix packages into your environment.
-  home.packages = [
-    pkgs.hello
-  ];
-
-
-  # Home Manager can also manage your environment variables through 'home.sessionVariables'. These will be explicitly sourced when using a shell provided by Home Manager.
-  home.sessionVariables = {
-    EDITOR = "code";
->>>>>>> parent of 538a506 (refactored files)
   };
 
   # Let Home Manager install and manage itself.
